d5f1f56160ddfb2c0ef6acdf16ff28a8679390c1
Szűcs <szgabbor@gmail.com>
2017-12-20 03:53:20 +0100

    added Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String result = "";
+        for (int i = 0; i < 1000; i++) {
+            result += "" + i+1 + ", ";
+        }
+        System.out.print(result);
+    }
+}
+    
+  
